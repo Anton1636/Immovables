c424b074cfa4a7152eeff04b8866328d0606ee9b
Anton1636 <you@example.comhobermac228@gmail.com>
2023-11-24 16:19:31 +0200

    Add connection to metamask, transaction between buyer and seller, and simple frontend

--- src/App.js
+++ src/App.js
@@ -1,31 +1,112 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
 
 // Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
+import Home from './components/Home'
+import Navigation from './components/Navigation'
+import Search from './components/Search'
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
 import Escrow from './abis/Escrow.json'
+import RealEstate from './abis/RealEstate.json'
 
 // Config
-import config from './config.json';
-
+import config from './config.json'
 function App() {
+	const [account, setAccount] = useState(null)
+	const [provider, setProvider] = useState(null)
+	const [escrow, setEscrow] = useState(null)
+	const [homes, setHomes] = useState([])
+	const [home, setHome] = useState([])
+	const [toggle, setToggle] = useState(false)
 
-  return (
-    <div>
+	const loadBlockchainData = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		setProvider(provider)
 
-      <div className='cards__section'>
+		const network = await provider.getNetwork()
 
-        <h3>Welcome to Millow</h3>
+		const realEstate = new ethers.Contract(
+			config[network.chainId].realEstate.address,
+			RealEstate,
+			provider
+		)
+		const totalSupply = await realEstate.totalSupply()
+		const homes = []
 
-      </div>
+		for (var i = 1; i <= totalSupply; i++) {
+			const uri = await realEstate.tokenURI(i)
+			const response = await fetch(uri)
+			const metadata = await response.json()
+			homes.push(metadata)
+		}
 
-    </div>
-  );
+		setHomes(homes)
+
+		const escrow = new ethers.Contract(
+			config[network.chainId].escrow.address,
+			Escrow,
+			provider
+		)
+		setEscrow(escrow)
+
+		window.ethereum.on('accountsChanged', async () => {
+			const accounts = await window.ethereum.request({
+				method: 'eth_requestAccounts',
+			})
+			const account = ethers.utils.getAddress(accounts[0])
+			setAccount(account)
+		})
+	}
+
+	useEffect(() => {
+		loadBlockchainData()
+	}, [])
+
+	const toggleProp = home => {
+		setHome(home)
+		toggle ? setToggle(false) : setToggle(true)
+	}
+
+	return (
+		<div>
+			<Search />
+			<Navigation account={account} setAccount={setAccount} />
+			<div className='cards__section'>
+				<h3>Homes for everyone</h3>
+				<hr />
+
+				<div className='cards'>
+					{homes.map((home, index) => (
+						<div className='card' key={index} onClick={() => toggleProp(home)}>
+							<div className='card__image'>
+								<img src={home.image} alt='Home' />
+							</div>
+							<div className='card__info'>
+								<h4>{home.attributes[0].value} ETH</h4>
+								<p>
+									<strong>{home.attributes[2].value}</strong> bds|
+									<strong>{home.attributes[3].value}</strong> ba|
+									<strong>{home.attributes[4].value}</strong> sqft|
+								</p>
+								<p>{home.address}</p>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+
+			{toggle && (
+				<Home
+					home={home}
+					provider={provider}
+					account={account}
+					escrow={escrow}
+					toggleProp={toggleProp}
+				/>
+			)}
+		</div>
+	)
 }
 
-export default App;
+export default App
